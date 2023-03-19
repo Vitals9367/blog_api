03a5820871d3b1327e70cfd4d5abd41d6d9d397f
Vitals9367 <vitalijus.alsauskas@gmail.com>
2023-03-19 18:13:15 +0200

    Docker build image workflow

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,9 @@
 from rest_framework import generics
 from blog.models import Post
 from blog.serializers import PostSerializer
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 # Create your views here.
-class PostList(generics.ListCreateAPIView):
+class PostList(generics.ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
-        
     
